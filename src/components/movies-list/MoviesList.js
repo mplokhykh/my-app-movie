8f0f9355f2af8b3164b81eeeba755f0540b97ff1
Maryna <plohih4@gmail.com>
2020-07-06 21:55:55 +0300

    LastUpdate

--- src/components/movies-list/MoviesList.js
+++ src/components/movies-list/MoviesList.js
@@ -6,6 +6,8 @@ import { Loading } from "../loading/Loading";
 import { PosterPreview } from "../poster-preview/PosterPreview";
 import { MovieCard } from "../movie-card/MovieCard";
 
+import './MoviesList.scss'
+
 export function MoviesList (props) {
     const { moviesList, isLoading, getMovies } = props;
 
@@ -18,14 +20,14 @@ export function MoviesList (props) {
 console.log(moviesList)
 
     return(
-    <div>
+    <div className='container'>
         {isLoading && <Loading />}
         {!isLoading && moviesList.map(item => {
            const poster_path = item.poster_path
             const release_year = item.release_date.substring(0,4)
             debugger
          return (
-             <div>
+             <div >
                  <PosterPreview poster_path={poster_path}/>
                  <MovieCard key={item.id} movie_title={item.title} release_year={release_year} rating={item.vote_average}/>
              </div>
